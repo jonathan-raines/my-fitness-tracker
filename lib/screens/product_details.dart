b347a9c0cfe843064d653167fe72a22faa6b29fa
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-01-29 16:03:22 -0600

    added ability to add more than one serving at a time

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -3,6 +3,8 @@ import 'package:openfoodfacts/model/Product.dart';
 import '../constants.dart';
 
 class ProductDetails extends StatelessWidget {
+  final myController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final Product product = ModalRoute.of(context).settings.arguments;
@@ -16,25 +18,42 @@ class ProductDetails extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.network('${product.imgSmallUrl}'),
+            SizedBox(
+              height: 30,
+            ),
             Text('Product Name: ${product.productName}'),
+            Text('Serving Size: ${product.servingSize}'),
             Text('Protein: ${product.nutriments.proteinsServing}'),
             Text('Carbs: ${product.nutriments.carbohydratesServing}'),
             Text('Fats: ${product.nutriments.fatServing}'),
-            Column(
-              children: [
-                TextButton(
-                  child: Text('Add Food'),
-                  onPressed: () {
-                    foodList.add(product);
-                    buildWidget(
-                        product.productName,
-                        product.nutriments.proteinsServing,
-                        product.nutriments.carbohydratesServing,
-                        product.nutriments.fatServing);
-                    Navigator.pushNamed(context, '/');
-                  },
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              height: 50,
+              width: 100,
+              child: TextField(
+                keyboardType: TextInputType.number,
+                controller: myController,
+                decoration: InputDecoration(
+                  hintText: 'Servings: ',
+                  border: OutlineInputBorder(),
                 ),
-              ],
+              ),
+            ),
+            TextButton(
+              child: Text('Add Food'),
+              onPressed: () {
+                var servingSize = int.parse(myController.text);
+
+                foodList.add(product);
+                buildWidget(
+                    product.productName,
+                    product.nutriments.proteinsServing * servingSize,
+                    product.nutriments.carbohydratesServing * servingSize,
+                    product.nutriments.fatServing * servingSize);
+                Navigator.pushNamed(context, '/');
+              },
             ),
           ],
         ),
