082b46f82d46cc8704f3c1175568e4412b64e023
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-02-03 15:32:42 -0600

    added all nutriments toMap() function

--- lib/services.dart
+++ lib/services.dart
@@ -1,3 +1,4 @@
+import 'package:openfoodfacts/model/Product.dart';
 import 'package:openfoodfacts/model/parameter/SearchTerms.dart';
 import 'package:openfoodfacts/model/parameter/TagFilter.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
@@ -40,9 +41,7 @@ Future<dynamic> productSearchKeywords(List<String> keywords) async {
     const SearchSimple(active: true),
     const SortBy(option: SortOption.POPULARITY),
     const TagFilter(
-        tagType: "countries",
-        contains: true,
-        tagName: "united states"),
+        tagType: "countries", contains: true, tagName: "united states"),
   ];
 
   ProductSearchQueryConfiguration configuration =
@@ -58,8 +57,59 @@ Future<dynamic> productSearchKeywords(List<String> keywords) async {
 }
 
 Map<String, dynamic> toMap(Product product) => {
-  'name': product.productName,
-  'protein': product.nutriments.proteinsServing,
-  'carbs': product.nutriments.carbohydratesServing,
-  'fats': product.nutriments.fatServing
-};
+      'barcode': product.barcode,
+      'productName': product.productName,
+      'brands': product.brands,
+      'nutriments': {
+        'saltServing': product.nutriments.saltServing,
+        'fiberServing': product.nutriments.fiberServing,
+        'sugarsServing': product.nutriments.sugarsServing,
+        'fatServing': product.nutriments.fatServing,
+        'saturatedFatServing': product.nutriments.saturatedFatServing,
+        'proteinsServing': product.nutriments.proteinsServing,
+        'energyServing': product.nutriments.energyServing,
+        'carbohydratesServing': product.nutriments.carbohydratesServing,
+        'caffeineServing': product.nutriments.caffeineServing,
+        'calciumServing': product.nutriments.calciumServing,
+        'ironServing': product.nutriments.ironServing,
+        'vitaminCServing': product.nutriments.vitaminCServing,
+        'phosphorusServing': product.nutriments.phosphorusServing,
+        'potassiumServing': product.nutriments.potassiumServing,
+        'sodiumServing': product.nutriments.sodiumServing,
+        'zincServing': product.nutriments.zincServing,
+        'copperServing': product.nutriments.copperServing,
+        'seleniumServing': product.nutriments.seleniumServing,
+        'vitaminAServing': product.nutriments.vitaminAServing,
+        'vitaminEServing': product.nutriments.vitaminEServing,
+        'vitaminDServing': product.nutriments.vitaminDServing,
+        'vitaminB1Serving': product.nutriments.vitaminB1Serving,
+        'vitaminB2Serving': product.nutriments.vitaminB2Serving,
+        'vitaminB6Serving': product.nutriments.vitaminB6Serving,
+        'vitaminB12Serving': product.nutriments.vitaminB12Serving,
+        'vitaminB9Serving': product.nutriments.vitaminB9Serving,
+        'vitaminKServing': product.nutriments.vitaminKServing,
+        'cholesterolServing': product.nutriments.cholesterolServing,
+        'magnesiumServing': product.nutriments.magnesiumServing,
+        'vitaminPPServing': product.nutriments.vitaminPPServing,
+        'alcoholServing': product.nutriments.alcoholServing,
+        'monounsaturatedServing': product.nutriments.monounsaturatedServing,
+        'polyunsaturatedServing': product.nutriments.polyunsaturatedServing,
+        'butyricAcidServing': product.nutriments.butyricAcidServing,
+        'capricAcidServing': product.nutriments.capricAcidServing,
+        'caproicAcidServing': product.nutriments.caproicAcidServing,
+        'caprylicAcidServing': product.nutriments.caprylicAcidServing,
+        'docosahexaenoicAcidServing':
+            product.nutriments.docosahexaenoicAcidServing,
+        'eicosapentaenoicAcidServing':
+            product.nutriments.eicosapentaenoicAcidServing,
+        'erucicAcidServing': product.nutriments.erucicAcidServing,
+        'lauricAcidServing': product.nutriments.lauricAcidServing,
+        'linoleicAcidServing': product.nutriments.linoleicAcidServing,
+        'myristicAcidServing': product.nutriments.myristicAcidServing,
+        'oleicAcidServing': product.nutriments.oleicAcidServing,
+        'palmiticAcidServing': product.nutriments.palmiticAcidServing,
+        'stearicAcidServing': product.nutriments.stearicAcidServing,
+      },
+      'servingSize': product.servingSize,
+      'servingQuantity': product.servingQuantity,
+    };
