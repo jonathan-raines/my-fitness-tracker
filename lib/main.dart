27b2f5c7ca2659460390677d04bd9a241993a8e4
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-02-08 11:14:24 -0600

    added custom food route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:my_fitness_tracker/screens/custom_food.dart';
 import 'package:my_fitness_tracker/screens/meal_page.dart';
 
 import 'screens/home_page.dart';
@@ -28,6 +29,7 @@ class NutritiOWN extends StatelessWidget {
         '/diary': (context) => Meals(),
         '/search': (context) => SearchPage(),
         '/details': (context) => ProductDetails(),
+        '/custom': (context) => CustomProductForm(),
       },
     );
   }
